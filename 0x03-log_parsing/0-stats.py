ac5887f3ed36dfb79823c0820ddad47be5bfe8e2
davidajimati <ajimatidavid@gmail.com>
2023-12-23 08:52:35 +0100

    0-

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,9 +2,7 @@
 """
 This script reads inputs from the stdin line by line and computes metrics
 """
-
 import sys
-
 cache = {'200': 0, '301': 0, '400': 0, '401': 0,
          '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
